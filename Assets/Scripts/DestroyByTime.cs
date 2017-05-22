019da764ea75e4957c31c4dae219a7b0ff27e5ed
samikiviniemi123 <sami.kiviniemi@Paakayttaja-iMac-2.local>
2017-05-22 11:38:24 +0300

    h

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour 
+{
+	public float lifeTime;
+	private float lifeTimeCounter;
+
+	void Start () 
+	{
+		lifeTimeCounter = lifeTime;
+	}
+
+	void Update () 
+	{
+		lifeTimeCounter -= Time.deltaTime;
+
+		if (lifeTimeCounter <= 0) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	Rigidbody rb;
+
+	private Camera mainCamera;
+
+	public GunController theGun;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		mainCamera = FindObjectOfType<Camera> ();
+	}
+	
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		Ray cameraRay = mainCamera.ScreenPointToRay (Input.mousePosition);
+		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
+		float rayLength;
+
+		if (groundPlane.Raycast (cameraRay, out rayLength)) {
+			Vector3 pointToLook = cameraRay.GetPoint (rayLength);
+
+			transform.LookAt (new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+		}
+		if (Input.GetMouseButtonDown (0)) {
+			theGun.isFiring = true;
+		}
+			if (Input.GetMouseButtonUp (0)) {
+				theGun.isFiring = false;
+			}
+	}
+}
